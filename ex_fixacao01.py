f102cd6f04e672bdf04a84ab351fa0761836468a
João Gabriel <joao.freiregabriel.2006@gmail.com>
2023-08-01 11:05:26 -0300

    exercicios de fixação em python

--- ex_fixacao01.py
+++ ex_fixacao01.py
@@ -1,8 +1,8 @@
 #Fazer um programa que pergunte um valor em dolares e converta para reais. Considere que 1 dolar vale 4,78 reais.
 
-din = float(input("Valor em dólar"))
+dindin = float(input("Valor em dólar"))
 
-real = (din)
+real = (dindin)
 dolar = 4.78
 
 conv = (real * dolar)
