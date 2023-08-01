5b18f06b31fb44adf37e16606543dbee12031e15
João Gabriel <joao.freiregabriel.2006@gmail.com>
2023-08-01 11:02:04 -0300

    exercicios de fixação

---
+++ ex_fixacao01.py
@@ -0,0 +1,10 @@
+#Fazer um programa que pergunte um valor em dolares e converta para reais. Considere que 1 dolar vale 4,78 reais.
+
+din = float(input("Valor em dólar"))
+
+real = (din)
+dolar = 4.78
+
+conv = (real * dolar)
+
+print("O valor em reais é R$",conv,)
