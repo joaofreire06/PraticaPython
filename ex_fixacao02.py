cde2010626815685f3d0c974b64ea1d16c500405
João Gabriel <joao.freiregabriel.2006@gmail.com>
2023-08-01 11:19:45 -0300

    exercicios de fixação em python 2

---
+++ ex_fixacao02.py
@@ -0,0 +1,9 @@
+#fazer um programa que pergunte o valor da conta a ser paga no restaurante . O pragrama deve apresentar como resposta o valor da conta com o acrescimo de 10% do garçom.
+
+vconta = float(input("Valor à ser pago"))
+
+valor = (vconta + vconta/10)
+
+print("Valor total R$",valor,)
+
+
